97e80e61e024715913f83d6fe751d3528dd56f0c
PCpau <pauds01@gmail.com>
2024-02-10 20:22:23 +0100

    h2o

---
+++ tutagua/aigua.sh
@@ -0,0 +1,4 @@
+#BUILDING PROTEIN SYSTEMS IN EXPLICIT WATER TUTORIAL
+#Visualitzem el fitxer pdb corresponent a Crystal structure of the extracellular domain of human RAMP1 amb vmd, podem visualitzar-ho directament amb:
+
+vmd 2yx8.pdb
