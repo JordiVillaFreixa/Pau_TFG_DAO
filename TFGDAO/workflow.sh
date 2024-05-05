e32a9e4a42f22c5dedea0d1fbb380c42e7c9bf2c
PCpau <pauds01@gmail.com>
2024-05-05 18:35:52 +0200

    act

--- TFGDAO/workflow.sh
+++ TFGDAO/workflow.sh
@@ -6,4 +6,12 @@ wget -nc https://files.rcsb.org/ligands/download/TPQ.cif
 
 #En relació a la trehalosa, serà més difícil ja que no es troba en format pdb o cif. Per otenir la seva estructura en 3D primer descarreguem desde drugbank la estructura en 3D-SDF, i a openbabel convertim el fitxer de .sdf a .pdb per poder treballar amb ell. El tenim anomenat com trehalose.pdb
 
-#Un cop tenim tots
+#Després d'obtenir el fitxer de la trehalosa observem que els residue names són UNL, per fer-ho més estètic i entenedor, amb el comando sed, ho canviem per TRE en un nou fitxer de manera que així no perdem l'original per si hem de tornar a treballar amb ell.
+
+ sed 's/UNL/TRE/' trehalose.pdb > tre.pdb
+
+#Un cop tenim tots els fitxers descarregats, ja podem començar a treballar amb ells. El primer de tot que hem de fer és fer un pdb4amber del nostre enzim, d'aquesta manera el fitxer serà modificat per poder ser utilitzat amb tleap que veurem més endavant.
+
+pdb4amber -i 1KSI.pdb -o dao.pdb --dry --reduce
+
+#Utilitzem dry i reduce per treure les aigües cristalografiques i afegir àtoms d'hidrogen a les localitzacions més òpptimes
