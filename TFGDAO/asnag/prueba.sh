429fc4be422fedf3d0772507966901339d0b3442
PCpau <pauds01@gmail.com>
2024-05-08 18:13:10 +0200

    del

--- TFGDAO/asnag/prueba.sh
+++ TFGDAO/asnag/prueba.sh
@@ -11,3 +11,6 @@ alguns àtoms són du
 pdb4amber -i asnnag.pdb -o nag.pdb
 antechamber -fi pdb -i nag.pdb -fo prepi -o nag.prepin -c bcc -at amber -nc 0 -rn AAG -bk AAG
 parmchk2 -i nag.prepin -f prepi -o frcmod.nag
+
+antechamber -j 5 -at sybyl -dr no -i aagt.pdb -o aagt.mol2 -fi pdb -fo mol2
+antechamber -j 5 -at sybyl -dr no -i aagt1.pdb -o aagt1.mol2 -fi pdb -fo mol2
