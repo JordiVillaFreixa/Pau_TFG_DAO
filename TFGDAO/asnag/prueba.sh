ea4bab1b248c96a04bcea008b7f659d0091e02e3
Pau Dominguez <pauds01@gmail.com>
2024-05-08 15:55:38 +0200

    asnag

---
+++ TFGDAO/asnag/prueba.sh
@@ -0,0 +1,13 @@
+#Fem el fitxer nou amb l'ajuda del Jordi
+#afegim hidrogens, amb openbabel es pot
+#li fem un pdb4amber (hem optat per aquesta)
+#li fem antechamber
+alguns àtoms són du
+
+#fem parmchk2
+
+#editar el pdb
+
+pdb4amber -i asnnag.pdb -o nag.pdb
+antechamber -fi pdb -i nag.pdb -fo prepi -o nag.prepin -c bcc -at amber -nc 0 -rn AAG -bk AAG
+parmchk2 -i nag.prepin -f prepi -o frcmod.nag
