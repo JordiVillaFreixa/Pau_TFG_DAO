2649f27765cbe0ae83af31c80e4f21bb1231bb25
Jordi Villà-Freixa <jordi.villa@uvic.cat>
2024-08-27 12:54:17 +0200

    summary figure

---
+++ analysis/rsync_output.sh
@@ -0,0 +1,2 @@
+rsync -Pchav --stats lavandula:/home/jvilla/scratch/ /Volumes/BackFam/calculs/lavandula 
+rsync -Pchav --stats lapalma:/storage/scratch/uvic24/uvic24774/ /Volumes/BackFam/calculs/lapalma 
