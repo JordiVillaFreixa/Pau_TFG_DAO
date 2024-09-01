ffad5a44817cb14ce07a1a18dd2b725b297c6b82
Jordi Villà-Freixa <jordi.villa@uvic.cat>
2024-09-01 13:00:06 +0200

    noves dades

--- analysis/rsync_output.sh
+++ analysis/rsync_output.sh
@@ -4,10 +4,18 @@ if [ -z "${BACKUPFOLDER}"]
 then
     echo "You should define the \$BACKUPFOLDER variable before runnning the script"
     exit
-else
+fi
+
+if [ "$#" -ne 1 ]
+then
     echo "\$BACKUPFOLDER defined and pointing at $BACKUPFOLDER"
     echo "############### rsyncing lavandula ####################"
     rsync -Pchav --stats lavandula:/home/jvilla/scratch/ $BACKUPFOLDER/lavandula 
     echo "############### rsyncing lapalma ####################"
     rsync -Pchav --stats lapalma:/storage/scratch/uvic24/uvic24774/ $BACKUPFOLDER/lapalma 
+    echo "############### rsyncing csuc ####################"
+    rsync -Pchav --stats csuc:/home/biotectfg02/scratch/ $BACKUPFOLDER/csuc 
+else
+    echo "############### rsyncing csuc ####################"
+    rsync -Pchav --stats csuc:/home/biotectfg02/scratch/ $BACKUPFOLDER/csuc
 fi
