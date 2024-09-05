e50cff8f18dac858682c405fccee16ac937b940d
Jordi Villà <jordi.villa@uvic.cat>
2024-09-05 08:58:19 +0200

    update

--- analysis/rsync_output.sh
+++ analysis/rsync_output.sh
@@ -8,15 +8,15 @@ fi
 
 if [ "$#" -ne 1 ]
 then
-    echo "\$BACKUPFOLDER defined and pointing at $BACKUPFOLDER"
-    echo "############### rsyncing lavandula ####################"
-    rsync -Pchav --stats lavandula:/home/jvilla/scratch/ $BACKUPFOLDER/lavandula 
-    echo "############### rsyncing lapalma ####################"
-    rsync -Pchav --stats lapalma:/storage/scratch/uvic24/uvic24774/ $BACKUPFOLDER/lapalma 
-    echo "############### rsyncing csuc ####################"
-    rsync -Pchav --stats csuc:/home/biotectfg02/scratch/ $BACKUPFOLDER/csuc 
+#    echo "\$BACKUPFOLDER defined and pointing at $BACKUPFOLDER"
+#    echo "############### rsyncing lavandula ####################"
+#    rsync -Pchav --stats lavandula:/home/jvilla/scratch/ $BACKUPFOLDER/lavandula 
+#    echo "############### rsyncing lapalma ####################"
+#    rsync -Pchav --stats lapalma:/storage/scratch/uvic24/uvic24774/ $BACKUPFOLDER/lapalma 
     echo "############### rsyncing pirineusIII ####################"
     rsync -Pchav --stats hpctest:/home/biotectfg02/scratch/ $BACKUPFOLDER/csuc 
+    echo "############### rsyncing csuc ####################"
+    rsync -Pchav --stats csuc:/home/biotectfg02/scratch/ $BACKUPFOLDER/csuc 
 
 else
     echo "############### rsyncing csuc ####################"
